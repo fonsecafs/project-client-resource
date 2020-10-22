db2dda3606749654046bdd21cc03df5fc3a4de26
Fabio Fonseca <fonseca.desenv@gmail.com>
2020-10-22 09:41:02 -0300

    Project created

---
+++ clientresource/src/main/java/com/project/clientresource/ClientresourceApplication.java
@@ -0,0 +1,13 @@
+package com.project.clientresource;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClientresourceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClientresourceApplication.class, args);
+	}
+
+}
---
+++ clientresource/src/test/java/com/project/clientresource/ClientresourceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.clientresource;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClientresourceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
