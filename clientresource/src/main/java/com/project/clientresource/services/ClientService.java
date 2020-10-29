7de8f1e2a24309dee1350e994dc465ae72d00cb4
Fabio Fonseca <fonseca.desenv@gmail.com>
2020-10-29 09:26:27 -0300

    Delete Cliente

--- clientresource/src/main/java/com/project/clientresource/services/ClientService.java
+++ clientresource/src/main/java/com/project/clientresource/services/ClientService.java
@@ -7,6 +7,7 @@ import java.util.stream.Collectors;
 import javax.persistence.EntityNotFoundException;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -51,8 +52,16 @@ public class ClientService {
 			Client entity = repository.getOne(id);
 			copyDTOToEntity(dto, entity);
 			entity = repository.save(entity);
-			return new ClientDTO(entity);		
+			return new ClientDTO(entity);
 		} catch (EntityNotFoundException e) {
+			throw new ResourceNotFountException("Id not found " + id);
+		}
+	}
+
+	public void delete(Long id) {
+		try {
+			repository.deleteById(id);
+		} catch (EmptyResultDataAccessException e) {
 			throw new ResourceNotFountException("Id not found " + id);
 		}
 	}
