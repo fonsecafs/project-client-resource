a0a1e8799ee38ab151e4300ef131140ddf8312de
Fabio Fonseca <fonseca.desenv@gmail.com>
2020-11-13 14:13:43 -0300

    Recreate ClientResourceApplication

---
+++ clientresource/src/main/java/com/project/clientresource/ClientResourceApplication.java
@@ -0,0 +1,13 @@
+package com.project.clientresource;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClientResourceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClientResourceApplication.class, args);
+	}
+
+}
